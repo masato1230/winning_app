5374613fc00262ccc8d62cbbaf1c4e4ec3d10c7f
Your Name <you@example.com>
2020-11-03 20:30:18 +0900

    create models

--- winning_app/models.py
+++ winning_app/models.py
@@ -3,14 +3,21 @@ from django.db import models
 class Thread(models.Model):
   thread_title = models.CharField(max_length=100)
   thread_desc = models.TextField(blank=True)
-  created_date = models.DateField(auto_now_add=True)
+  created_date = models.DateTimeField(auto_now_add=True)
   tags = models.TextField(blank=True)
+
+  def __str__(self):
+    return self.thread_title
 
 class Comment(models.Model):
   thread_id = models.IntegerField(null=False)
   comment = models.CharField(max_length=100)
-  goods = models.IntegerField(blank=True)
-  replies = models.IntegerField(blank=True)
+  created_time = models.DateTimeField(auto_now_add=True)
+  goods = models.IntegerField(blank=True, default=0)
+  replies = models.IntegerField(blank=True, default=0)
+
+  def __str__(self):
+    return self.comment
 
 
 
