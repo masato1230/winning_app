98ecd39917665844313e532c7cf77883aec82cef
Your Name <you@example.com>
2020-11-03 20:21:34 +0900

    before migration

--- winning_app/models.py
+++ winning_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Thread(models.Model):
+  thread_title = models.CharField(max_length=100)
+  thread_desc = models.TextField(blank=True)
+  created_date = models.DateField(auto_now_add=True)
+  tags = models.TextField(blank=True)
+
+class Comment(models.Model):
+  thread_id = models.IntegerField(null=False)
+  comment = models.CharField(max_length=100)
+  goods = models.IntegerField(blank=True)
+  replies = models.IntegerField(blank=True)
+
+
+
+
+
